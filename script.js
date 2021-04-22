5f48283d4bc3eeeb8396f0f415b49dddb481d925
colmo0912 <colmo0912@gmail.com>
2021-04-22 15:24:46 +0200

    aggiungo nuovo oggetto sentro l'array con push

--- script.js
+++ script.js
@@ -22,16 +22,19 @@ var gruppoStudenti = [
 
     studente1 = {
         nome:"Andrea",
-        cognome:"Moi"
+        cognome:"Moi",
+        eta: 39
     },
     studente2 = {
         nome:"Mario",
-        cognome:"Bianchi"
+        cognome:"Bianchi",
+        eta: 20
     },
 
     studente3 = {
         nome:"Carlo",
-        cognome:"Rossi"
+        cognome:"Rossi",
+        eta: 25
     }
 
 
@@ -46,8 +49,17 @@ for (var i = 0; i<gruppoStudenti.length; i++){
 }
 
 //creo i 3 prompt e le variabili in cui salvo i dati inseriti dall'utente
+//i prompt sono all'interno di un nuovo oggetto e gli imput sono i valori che saranno associtati
+// a nome, cognome ed età
 
-var nuovoStudenteNome = prompt("Digita il nome del nuovo studente")
-var nuovoStudenteCognome = prompt("Digita il cognome del nuovo studente")
-var nuovoStudenteEta = prompt("Digita l'età del nuovo studente")
-console.log("Il nuovo sudente si chiama " + nuovoStudenteNome + " " + nuovoStudenteCognome + " e ha " + nuovoStudenteEta + " anni")
+var stutente4 = {
+
+    nome : prompt("Digita il nome del nuovo studente"),
+    cognome : prompt("Digita il cognome del nuovo studente"),
+    eta : prompt("Digita l'età del nuovo studente")
+    
+}
+
+
+
+console.log(gruppoStudenti.push(stutente4))
